7e398713d515bb32532740cff279f7f09c90c407
serega1993 <serega_10.10.93@mail.ru>
2018-01-16 16:19:36 +0300

    Decorator done.

---
+++ StructuralPatterns/Decorator/SwitzerlandProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    class SwitzerlandProduct : Product
+    {
+        public override void ShowProduct()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("fr-CH");
+            base.ShowProduct();
+        }
+
+    }
+}
