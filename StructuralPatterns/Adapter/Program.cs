be40f8b65e5382a54675507a4e7a3e3044fe6285
serega1993 <serega_10.10.93@mail.ru>
2018-01-22 16:57:33 +0300

    Adapter modified accordint to the review.

--- StructuralPatterns/Adapter/Program.cs
+++ StructuralPatterns/Adapter/Program.cs
@@ -11,7 +11,12 @@ namespace Adapter
         static void Main(string[] args)
         {
             var str = new StringAdapter { InputString = "My mother tongue is Russian." };
-            str.CountLetters(new char[] {'a', 'm', 'o'});
+            var letters = new char[] { 'a', 'm', 'o' };
+            var count = str.CountLetters(letters);
+
+            for (int i = 0; i < letters.Length; i++)
+                Console.WriteLine($"{letters[i]} - {count[i]} time(s).");
+
 
             Console.ReadKey();
         }
