f4b35b7874ddd8945e3a8bfc30e42c5d32e16b46
serega1993 <serega_10.10.93@mail.ru>
2018-01-25 09:36:39 +0300

    Task5 done.

---
+++ Task5/ObserverPattern/CollectionIntObserver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObserverPattern
+{
+    class CollectionIntObserver : IObserver<CollectionInt>
+    {   
+        public CollectionIntObserver()
+        {            
+        }
+        
+        public void OnNext(CollectionInt collectionInt)
+        {
+            collectionInt.Sort();
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
