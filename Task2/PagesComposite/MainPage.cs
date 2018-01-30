8433cab34098037b8f58826a3cbaa043f5617311
serega1993 <serega_10.10.93@mail.ru>
2018-01-30 20:59:05 +0300

    Task2 done.

---
+++ Task2/PagesComposite/MainPage.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagesComposite
+{
+    internal class MainPage : Page
+    {
+        private readonly IList<Page> _directoryPages;
+
+        public MainPage(string title)
+        {
+            _directoryPages = new List<Page>();
+            Title = title;
+            Head = @"<meta content='text / html'; charset = 'windows - 1251' />";
+        }
+
+        public DirectoryPage AddPage(string title)
+        {
+            var x = new DirectoryPage { Title = title};
+            _directoryPages.Add(x);
+            return x;
+        }
+        public DirectoryPage AddPage(string title, string body)
+        {
+            var x = new DirectoryPage {Title = title, Body = body};
+            _directoryPages.Add(x);
+            return x;
+        }
+
+        public override string ShowPage()
+        {
+            var result = $"MAIN PAGE{Environment.NewLine}" +
+                         $"{Title}{Environment.NewLine}" +
+                         $"<head>{Environment.NewLine}" +
+                         $"{Head}{Environment.NewLine}" +
+                         $"</head>{Environment.NewLine}" +
+                         $"<body>{Environment.NewLine}" +
+                         $"{Body}{Environment.NewLine}" +
+                         $"</body>{Environment.NewLine}" +
+                         $"---------------------------{Environment.NewLine}";
+            return result;
+        }
+
+        public string ShowWebSite()
+        {
+            var result = ShowPage();
+            foreach (var item in _directoryPages)
+            {
+                result += item.ShowPage();
+                result += ((DirectoryPage)item).ShowPages();
+            }
+            return result;
+        }
+    }
+}
---
+++ Task2/PagesComposite/SimplePage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagesComposite
+{
+    class SimplePage : Page
+    {
+        public SimplePage()
+        {
+            Head = @"<meta content='text / html'; charset = 'windows - 1251' />";
+        }
+
+        public override string ShowPage()
+        {
+            var result = $"\t\t{Title}{Environment.NewLine}" +
+                         $"\t\t<head>{Environment.NewLine}" +
+                         $"\t\t{Head}{Environment.NewLine}" +
+                         $"\t\t</head>{Environment.NewLine}" +
+                         $"\t\t<body>{Environment.NewLine}" +
+                         $"\t\t{Body}{Environment.NewLine}" +
+                         $"\t\t</body>{Environment.NewLine}" +
+                         $"\t\t---------------------------{Environment.NewLine}";
+            return result;
+        }
+    }
+}
